a2a5baa049cf93c6cca051ce061f53cdadec56ed
Swastik Mukherjee <82890981+aarush202070@users.noreply.github.com>
2024-08-29 01:02:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function next()
+{
+    window.location = "home.html";
+}
+function ROI()
+{
+    window.location = "crop return.html";
+}
+function WE()
+{
+    window.location = "crop growth advisor.html";
+}
+function SS()
+{
+    window.location = "supplies.html";
+}
+function BB()
+{
+    window.location = "buyers.html";
+}
+function HE()
+{
+    window.location = "home.html";
+}
